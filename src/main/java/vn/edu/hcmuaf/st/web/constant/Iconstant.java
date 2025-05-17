09736e2c60d13b2fe0c905159487c94143332c46
HoQuocBao <22130018@st.hcmuaf.edu.vn>
2025-05-18 00:28:34 +0700

    Remove secret

---
+++ src/main/java/vn/edu/hcmuaf/st/web/constant/Iconstant.java
@@ -0,0 +1,15 @@
+package vn.edu.hcmuaf.st.web.constant;
+
+public class Iconstant {
+    public static final String GOOGLE_CLIENT_ID = "783371694466-v4tsjt4ch1ulqtbdinv02of0q3t4tpm8.apps.googleusercontent.com";
+
+    public static final String GOOGLE_CLIENT_SECRET = "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";
+
+    public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/web/login";
+
+    public static final String GOOGLE_GRANT_TYPE = "authorization_code";
+
+    public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+    public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+}
